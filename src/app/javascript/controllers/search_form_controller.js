02c414820a7b5841a77a3cb58aae9e9f9b1d171f
Fabrice David <fabrice.david@epfl.ch>
2026-03-11 06:47:42 -0400

    improving text search field in search projects form

--- src/app/javascript/controllers/search_form_controller.js
+++ src/app/javascript/controllers/search_form_controller.js
@@ -1,34 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = {
-    debounceMs: { type: Number, default: 350 }
-  }
-
-  connect() {
-    this.submitTimer = null
-  }
-
-  disconnect() {
-    this.clearSubmitTimer()
-  }
-
-  submitDebounced() {
-    this.clearSubmitTimer()
-    this.submitTimer = setTimeout(() => {
-      this.submitNow()
-    }, this.debounceMsValue)
-  }
-
   submitNow() {
-    this.clearSubmitTimer()
     this.element.submit()
-  }
-
-  clearSubmitTimer() {
-    if (this.submitTimer) {
-      clearTimeout(this.submitTimer)
-      this.submitTimer = null
-    }
   }
 }

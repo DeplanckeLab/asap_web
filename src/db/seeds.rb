14eb6f0f18e7ff3979e40b81c074b1811fbb4033
Fabrice David <fabrice.david@epfl.ch>
2026-03-12 03:23:58 -0400

    adding row_label and col_label properly in db:seed

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Seeding project type labels..."
+
+project_type_labels = {
+  "Single-cell transcriptomics" => { row_label: "genes", col_label: "cells" },
+  "Bulk transcriptomics" => { row_label: "genes", col_label: "samples" }
+}
+
+project_type_labels.each do |name, labels|
+  project_type = ProjectType.find_by(name: name)
+
+  unless project_type
+    puts "ProjectType not found: #{name}"
+    next
+  end
+
+  project_type.assign_attributes(labels)
+
+  if project_type.changed?
+    project_type.save!
+    puts "Updated #{name}: row_label=#{project_type.row_label}, col_label=#{project_type.col_label}"
+  else
+    puts "No change for #{name}"
+  end
+end

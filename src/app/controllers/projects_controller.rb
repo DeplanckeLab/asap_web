a79ba3f186b47e5b73c9b30645a6b2e6f385a9ed
Fabrice David <fabrice.david@epfl.ch>
2026-03-13 08:49:30 -0400

    sort projects by update_at

--- src/app/controllers/projects_controller.rb
+++ src/app/controllers/projects_controller.rb
@@ -39,7 +39,7 @@ class ProjectsController < ApplicationController
       tissue: params[:tissue],
       status_id: params[:status_id],
       visibility: visibility,
-      sort: params[:sort] || 'created_at',
+      sort: params[:sort] || 'updated_at',
       page: params[:page] || 1,
       # User permission context for filtering
       current_user_id: current_user&.id,

bd7cb6945a19addb2f6f720a3c5845875f52e756
Fabrice David <fabrice.david@epfl.ch>
2026-06-22 15:16:57 -0400

    Stop tracking tmp/ directory

--- tmp/docker-rufo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Get the file path that needs formatting
-FILE_PATH=$1
-
-# Run rufo inside the Docker container
-# Replace 'your-container-name' with your actual container name
-docker exec your-container-name rufo "$FILE_PATH" 
